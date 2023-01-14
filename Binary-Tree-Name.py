a06d05a4ac248f4b28c4af84e941bd5da3a6a98d
EsmeraldaUriel <116430431+EsmeraldaUriel@users.noreply.github.com>
2023-01-15 02:12:58 +0800

    "Checking the value using if else statement"

--- Binary-Tree-Name.py
+++ Binary-Tree-Name.py
@@ -4,3 +4,19 @@ class BinarySearchTreeNode:
         self.left = None
         self.right = None
 
+    def add_child(self, data):
+        if data == self.data:  # check if the value exist
+            return  # if the value exist simply return
+
+        if data < self.data:  # allow to add the data in the left subtree
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+
+
